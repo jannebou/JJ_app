8a9833926d74353dcfe2f17985f56e990af6f1d7
Janne <Janne.sainio15@gmail.com>
2026-06-09 20:49:56 +0300

    bwq

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781026892|788376275';
+const CACHE_VERSION = '1781027389|1284794770';
 /** @type {string} */
 const CACHE_PREFIX = 'jj-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
